138b4adb28e4f5fb033c08e70ead595591d29c54
jakobhero <jakob.herold.jh@gmail.com>
2021-12-12 11:54:10 +0000

    Test Commit more readable Syntax for data-analytics

--- src/components/post-content-layout.svelte
+++ src/components/post-content-layout.svelte
@@ -115,7 +115,9 @@
             href={link.href}
             rel="noreferrer"
             target="_blank"
-            data-analytics="&lcub&quotvariant&quot:&quotsocial_media&quot,&quotcontext&quot:&quot{link.alt.toLowerCase()}_share&quot&rcub"
+            data-analytics={`&lcub"variant":"social_media","context":"` +
+              link.alt.toLowerCase() +
+              `_share"&rcub`}
           >
             <img src={link.icon} alt={link.alt} height="24" width="24" />
           </a>
